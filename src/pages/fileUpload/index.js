a767336f7e0ccb981b640e5a7e75e449afcde09b
Mist0ne <57776514+Mist0ne@users.noreply.github.com>
2023-05-09 16:29:45 +0300

    v 1.1

--- src/pages/fileUpload/index.js
+++ src/pages/fileUpload/index.js
@@ -1,11 +1,59 @@
-import {Box, Container} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
+import {useState} from "react";
+import {FileUploader} from "react-drag-drop-files";
+import {useTranslation} from "react-i18next";
+import Button from "@mui/material/Button";
+
 
 const FileUploadPage = () => {
+    const {t} = useTranslation();
 
-    return <Container>
-        <Box>
-            test
-        </Box>
+    const fileTypes = ["PDF", "RAR", "ZIP"];
+    const [files, setFiles] = useState([]);
+
+    const handleChange = (newFiles) => {
+        const map = new Map();
+
+        [...newFiles, ...files].forEach((file) => {
+            map.set(file['name'], file);
+        });
+
+        setFiles([...map.values()]);
+    };
+
+    const formSubmit = (e) => {
+        console.log(e);
+    };
+
+    return <Container
+        maxWidth="md"
+        sx={{display: 'flex', flexFlow: 'column nowrap', alignItems: 'center', boxShadow: 1}}
+    >
+        <form onSubmit={formSubmit}>
+            <Box>
+                <Typography variant="h4" sx={{paddingY: 6}}>
+                    {t('fileUploadPage.title')}
+                </Typography>
+                <FileUploader
+                    multiple={true}
+                    required={true}
+                    handleChange={handleChange}
+                    name="files"
+                    types={fileTypes}
+                />
+                <Typography variant="h5" sx={{paddingTop: 4}}>
+                    {files.length ? t('fileUploadPage.uploader.uploadedList') : t('fileUploadPage.uploader.nothingToShow')}
+                </Typography>
+                <ul>
+                    {files.map((file) => <li key={file.name}>{file.name}</li>)}
+                </ul>
+            </Box>
+            <Box sx={{display: 'flex', flexFlow: 'column nowrap', alignItems: 'center', paddingY: 6}}>
+                <Button type="submit" size="large" variant="contained">
+                    {t('fileUploadPage.send')}
+                </Button>
+            </Box>
+        </form>
     </Container>;
 };
 
