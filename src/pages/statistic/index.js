01d56bf79e6442863886ee0ac97c496938041427
Mist0ne <57776514+Mist0ne@users.noreply.github.com>
2023-05-20 23:01:13 +0300

    statistics page

--- src/pages/statistic/index.js
+++ src/pages/statistic/index.js
@@ -1,10 +1,112 @@
-import {Box, Container} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
+import axiosWrapper from "../../utils/axios";
+import {useEffect, useState} from "react";
+import {DataGrid} from '@mui/x-data-grid';
+import {useTranslation} from "react-i18next";
 
 const StatisticPage = () => {
+    const {t} = useTranslation();
+
+    const columns = [
+        {
+            field: 'fileId',
+            headerName: 'ID',
+            width: 450
+        },
+        {
+            field: 'checkResult',
+            headerName: t('statisticPage.allowed'),
+            width: 250,
+            valueGetter: (params) => t(`statisticPage.statuses.${params.value}`)
+        },
+        {
+            field: 'checkDetails',
+            headerName: t('statisticPage.details'),
+            sortable: false,
+            width: 400,
+            valueGetter: (params) =>
+                params.value
+                    ? t('statisticPage.downloadJsonButton')
+                    : t('statisticPage.noDetails')
+        },
+    ];
+
+    const downloadObjectAsJson = (exportObj, exportName) => {
+        const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(exportObj);
+        const downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute("href", dataStr);
+        downloadAnchorNode.setAttribute("download", exportName + ".json");
+        document.body.appendChild(downloadAnchorNode);
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
+    }
+
+
+    const [files, setFiles] = useState([]);
+
+    const dataProcessing = (data) => {
+        let newFiles = [];
+        Object.keys(data).forEach((key, index) => {
+            if (!key.includes('_results') && typeof data[key] === 'boolean') {
+                newFiles.push({
+                    id: index + 1,
+                    fileId: key,
+                    checkResult: data[key],
+                    checkDetails: data[`${key}_results`],
+                });
+            } else if (data[key] === 'in progress') {
+                newFiles.push({
+                    id: index + 1,
+                    fileId: key,
+                    checkResult: 'inProgress',
+                    checkDetails: null,
+                });
+            } else if (data[key] === 'unknown task id') {
+                newFiles.push({
+                    id: index + 1,
+                    fileId: key,
+                    checkResult: 'unknownFile',
+                    checkDetails: null,
+                });
+            }
+        });
+        setFiles(newFiles);
+    };
+
+    useEffect(() => {
+        axiosWrapper('get', 'statistics', {}, dataProcessing);
+    }, []);
 
     return <Container>
         <Box>
-            test
+            <Box sx={{my: 4, display: 'flex', justifyContent: 'center'}}>
+                <Typography variant="h5">
+                    {t('statisticPage.title')}
+                </Typography>
+            </Box>
+            {files.length
+                ? <DataGrid
+                    rows={files}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {page: 0, pageSize: 10},
+                        },
+                    }}
+                    pageSizeOptions={[5, 10, 20]}
+                    checkboxSelection
+                    onCellClick={(params) => {
+                        if (params.field === 'checkDetails') {
+                            const checkedFile = files[params.id - 1];
+                            if (checkedFile.checkDetails)
+                                downloadObjectAsJson(checkedFile.checkDetails, checkedFile.fileId);
+                        }
+                    }}
+                />
+                : <Typography variant="h7" sx={{display: 'flex', justifyContent: 'center'}}>
+                    {t('statisticPage.nothingToShow')}
+                </Typography>
+            }
         </Box>
     </Container>;
 };
